86719ebcbc9f16cefeb7986dbd5c45277b6965f4
iammohammadikram <147922978+iammohammadikram@users.noreply.github.com>
2024-09-28 11:35:51 +0530

    Create linearSearch.java

---
+++ linearSearch.java
@@ -0,0 +1,14 @@
+Class linearSearch{
+  public Static void main(){
+    int a[]={1,2,7,9,4,55};
+    int find=6;
+    for(i=0; i<find ; i++){
+        if( a[i] == find){
+            System.out.println("the value is present in the list");
+        }
+      else{
+            System.out.println("the value is not present in the list");
+      }
+    }
+  }
+}
